ac48734565746789ac4e7964e681b8088d8df9ab
akash varma <akashvarma@smartdatainc.net>
2024-09-04 18:27:21 +0530

    sidebar and layout

--- Layout_Ng/src/app/layout/private/private-routes/private-routes.component.ts
+++ Layout_Ng/src/app/layout/private/private-routes/private-routes.component.ts
@@ -3,14 +3,19 @@ import { HeaderComponent } from '../../../shared/component/header/header.compone
 import { FooterComponent } from '../../../shared/component/footer/footer.component';
 import { SidebarComponent } from '../../../shared/component/sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-private-routes',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, SidebarComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, SidebarComponent, RouterOutlet, MatIconModule],
   templateUrl: './private-routes.component.html',
-  styleUrl: './private-routes.component.scss'
+  styleUrl: './private-routes.component.scss',
 })
 export class PrivateRoutesComponent {
+  isSidebarClosed = false;
 
+  toggleSidebar() {
+    this.isSidebarClosed = !this.isSidebarClosed;
+  }
 }
